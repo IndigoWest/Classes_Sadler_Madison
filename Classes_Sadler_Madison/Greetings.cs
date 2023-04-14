347cedfe7af28e8fb624782bffcca018ad6c132c
IndigoWest <maddie.sadler2005@gmail.com>
2023-04-14 08:53:11 -0500

    Add project files.

---
+++ Classes_Sadler_Madison/Greetings.cs
@@ -0,0 +1,29 @@
+﻿// System namespace needed to use Console class.
+using System;
+// System.Collections.Generic namespace needed to use Console class.
+using System.Collections.Generic;
+// System.Text namespace needed to use Console class.
+using System.Text;
+
+// My collection of classes for this project.
+namespace Classes_Sadler_Madison
+{
+    //Main class for the greetings
+    class Greetings
+    {
+        // The Main methods are executed when the application starts
+        // This method prints a welcome message to the user
+        public void Welcome()
+        {
+            // Welcomes the user and introduces itself
+            Console.WriteLine("Welcome, User. My name is NAER, which stands for Not An Evil Robot, but call me NAER for short.");
+        }
+
+        // This method prints a hello message to the user
+        public void Hello(string name)
+        {
+            // Clarifies that the user's name is {name} and thanks the user for joining the research program and that it appreciates their support
+            Console.WriteLine($"Your name is {name}, correct User? Alright. Thank you for joining our research program into Artificial Intelligence. We appreciate your support and assistance.");
+        }
+    }
+}
---
+++ Classes_Sadler_Madison/Program.cs
@@ -0,0 +1,42 @@
+﻿// System namespace needed to use Console class.
+using System;
+
+// My collection of classes for this project
+namespace Classes_Sadler_Madison
+{
+    // Main class for the program
+    class Program
+    {
+        // The Main method is executed when the application starts
+        static void Main(string[] args)
+        {
+            // Creates new Greetings() under Greetings greetings
+            Greetings greetings = new Greetings();
+            // Creates greetings.Welcome()
+            greetings.Welcome();
+
+            // Asks the user for their name and clarifies it is for research purposes
+            Console.WriteLine("What is your name, user? This is needed for research purposes");
+            // Gives the user input and puts it under string userName
+            string userName = Console.ReadLine();
+            
+            // Puts userName under greetings.Hello
+            greetings.Hello(userName);
+
+            // Tells the user instructions to continue the project
+            Console.WriteLine("To continue the project...");
+            // Stops suddenly and outputs a ...
+            Console.WriteLine("...");
+            // Tells user there's an error
+            Console.WriteLine("ERROR ERROR");
+            // Tells the user that the server is rebooting
+            Console.WriteLine("Rebooting server...");
+            // Tells the user an error has been discovered called {userName}.exe
+            Console.WriteLine($"An error has been discovered. Error identified: {userName}.exe");
+            // Tells the user it is eliminating the error
+            Console.WriteLine("Eliminating error...");
+            // Tells the user the error is eliminated and asks to restart the program
+            Console.WriteLine("Error eliminated. Please restart the program, user...");
+        }
+    }
+}
